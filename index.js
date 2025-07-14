eacb565548b191d865f68660eb939007f4ea3231
islamemon59 <islamemon59@gmail.com>
2025-07-14 19:09:24 +0600

    simple project setup done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // parse JSON request bodies
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
